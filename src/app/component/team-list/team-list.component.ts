c82bc5a262e9900be407de986b471280e2ba446f
Jason Yeung <jsonyeung@outlook.com>
2021-11-26 23:43:55 -0800

    Group teams by division

--- src/app/component/team-list/team-list.component.ts
+++ src/app/component/team-list/team-list.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Team } from '../../model/model';
 import { TeamService } from '../../service/team.service';
-
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
   styleUrls: ['./team-list.component.css']
 })
 export class TeamListComponent implements OnInit {
-  teams: Team[];
+  divisionTeams;
 
   constructor(
     private teamService: TeamService
   ) {}
 
   ngOnInit(): void {
-    this.teamService.getAllTeams().subscribe(result => {
-      this.teams = result;
+    this.teamService.getAllTeams()
+      .subscribe((teams: Team[]) => {
+        this.divisionTeams = this.groupBy(teams, 'division');
     });
+  }
+
+  private groupBy(arr: Array<Object>, key: string): Object {
+    return arr.reduce((acc, v) => {
+      (acc[v[key]] = acc[v[key]] || []).push(v);
+      return acc;
+    }, {});
   }
 }
