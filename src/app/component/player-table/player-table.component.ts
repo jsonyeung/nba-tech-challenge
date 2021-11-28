dadc4df88e9d3e4c0d8a0ab281f8c78b29c50b07
Jason Yeung <jsonyeung@outlook.com>
2021-11-27 23:01:28 -0800

    Implement sorting via ngx-easy-table

--- src/app/component/player-table/player-table.component.ts
+++ src/app/component/player-table/player-table.component.ts
@@ -1,12 +1,31 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Player } from 'src/app/model/model';
+import { Columns, Config, DefaultConfig } from 'ngx-easy-table';
 
 @Component({
   selector: 'player-table',
   templateUrl: './player-table.component.html',
   styleUrls: ['./player-table.component.scss']
 })
-export class PlayerTableComponent {
-  constructor() { }
+export class PlayerTableComponent implements OnInit{
   @Input() data: Player[];
+  columns: Columns[];
+  config: Config;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.config = { 
+      ...DefaultConfig,
+      rows: 25
+    };
+
+    this.columns = [
+      { key: 'firstName', title: 'First Name' },
+      { key: 'lastName', title: 'Last Name' },
+      { key: 'weightPounds', title: 'WT (lb)' },
+      { key: 'heightInches', title: 'HT (in)' },
+      { key: 'position', title: 'Pos' }
+    ];
+  }
 }
